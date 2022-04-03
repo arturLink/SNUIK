f5d9755fcfeb1e4857847f5b80c1fe0f13280341
vafle <vafle@DESKTOP-K2BF7TN>
2022-04-03 18:05:29 +0300

    ZVUK

--- snake/Params.cs
+++ snake/Params.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace snake
-{
-    class Params
-    {
-        private string resourcesFolder;
+//namespace snake
+//{
+//    class Params
+//    {
+//        private string resourcesFolder;
 
-        public Params()
-        {
-            var ind = Directory.GetCurrentDirectory().ToString().IndexOf("bin", StringComparison.Ordinal);
+//        public Params()
+//        {
+//            var ind = Directory.GetCurrentDirectory().ToString().IndexOf("bin", StringComparison.Ordinal);
 
-            string binFolder = Directory.GetCurrentDirectory().ToString().Substring(0, ind).ToString();
-            resourcesFolder = binFolder + "resources\\";
-        }
+//            string binFolder = Directory.GetCurrentDirectory().ToString().Substring(0, ind).ToString();
+//            resourcesFolder = binFolder + "resources\\";
+//        }
 
-        public string GetResourcesFolder()
-        {
-            return resourcesFolder;
+//        public string GetResourcesFolder()
+//        {
+//            return resourcesFolder;
 
-        }
-    }
-}
+//        }
+//    }
+//}
--- snake/Sound.cs
+++ snake/Sound.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WMPLib;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using WMPLib;
 
-namespace snake
-{
-    class Sound
-    {
-        WindowsMediaPlayer player = new WindowsMediaPlayer();
-        private string pathToMedia;
+//namespace snake
+//{
+//    class Sound
+//    {
+//        WindowsMediaPlayer player = new WindowsMediaPlayer();
+//        private string pathToMedia;
 
-        public Sounds(string pathToResources)
-        {
-            pathToMedia = pathToResources;
-        }
+//        public Sounds(string pathToResources)
+//        {
+//            pathToMedia = pathToResources;
+//        }
 
-        public void Play()
-        {
-            player.URL = pathToMedia + "bin.mp3";
-            player.settings.volume = 30;
-            player.controls.play();
-            player.settings.setMode("loop", true);
-        }
+//        public void Play()
+//        {
+//            player.URL = pathToMedia + "bin.mp3";
+//            player.settings.volume = 30;
+//            player.controls.play();
+//            player.settings.setMode("loop", true);
+//        }
 
-        public void Play(string songName)
-        {
-            player.URL = pathToMedia + songName + ".mp3";
-            player.controls.play();
-        }
+//        public void Play(string songName)
+//        {
+//            player.URL = pathToMedia + songName + ".mp3";
+//            player.controls.play();
+//        }
 
-        public void PlayEat()
-        {
-            player.URL = pathToMedia + "click.mp3";
-            player.settings.volume = 100;
-            player.controls.play();
-        }
-    }
-}
+//        public void PlayEat()
+//        {
+//            player.URL = pathToMedia + "click.mp3";
+//            player.settings.volume = 100;
+//            player.controls.play();
+//        }
+//    }
+//}
